238d0caaf5636503ca3701554f4776f927d38374
KetiElizbarashvili <elizbarashviliketi44@gmail.com>
2023-10-18 03:03:18 +0400

    projectstart

---
+++ FoodOrderingApp/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import {StyleSheet, Text, View, Button, ScrollView, SafeAreaView} from 'react-native';
+
+export default function App() {
+  return (
+      <SafeAreaView>
+    <View >
+       <Text>
+       </Text>
+
+    </View>
+      </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
